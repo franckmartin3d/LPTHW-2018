a89138dcf163669b96eb49acc8b7b48c4080b13f
Francois Martin <f_martin@wargaming.net>
2018-03-01 18:35:45 +1100

    do it

--- ex18_NamesVariablesCodeFunction.py
+++ ex18_NamesVariablesCodeFunction.py
@@ -14,7 +14,7 @@ def print_two(*args):
 #ok , that *args is actually pointless, we can just do this
 
 def print_two_again(arg1, arg2):
-    print("arg1: {arg1} , arg2: {arg2}")
+    print(f"arg1: {arg1} , arg2: {arg2}")
 
 print_two("Zed", "shaw")
 print_two_again("Zed2","shaw2")
