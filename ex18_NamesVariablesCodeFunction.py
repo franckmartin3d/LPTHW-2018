1e6e4f04ecaea8c901cb92f39b47e39c760330fb
Francois Martin <f_martin@wargaming.net>
2018-03-01 18:30:38 +1100

    do it

---
+++ ex18_NamesVariablesCodeFunction.py
@@ -0,0 +1,20 @@
+# Franck Martin
+#1/03/2018
+
+#function do 3 things
+# 1 they name piece of code the way variables names strings and numbers.
+# 2 they take arghuments the way your scripts take argv
+# 3 Using 1 and 2 they let you make your own "mini scripts" or tiny commands."
+
+#This one is like your scripts with argv
+def print_two(*args):
+    arg1, arg2 = args
+    print(f"arg1: {arg1} , arg2: {arg2}")
+
+#ok , that *args is actually pointless, we can just do this
+
+def print_two_again(arg1, arg2):
+    print("arg1: {arg1} , arg2: {arg2}")
+
+print_two("Zed", "shaw")
+print_two_again("Zed2","shaw2")
