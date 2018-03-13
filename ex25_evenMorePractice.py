083ea1d1e29e4f8fdb74676a808e0284dd5298e9
franckmartin3d <franck.martin3d@gmail.com>
2018-03-14 00:36:20 +1100

    20 to 25

---
+++ ex25_evenMorePractice.py
@@ -0,0 +1,11 @@
+#franck 14/03/2018
+def break_words(stuff):
+    #this function will break up words for us
+    words = stuff.split(' ')
+    return words
+
+def sort_words(words):
+    #sorts the words.
+    return sorted(words)
+
+def
